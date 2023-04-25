bf6b0e289a9bd28811fd30dfc195e8e6f4c3bd53
K_Pal <K_Pal@dell.com>
2023-04-25 11:24:10 +0530

    till 8th point

--- 2_demo/src/main/java/com/example/demo/Alien.java
+++ 2_demo/src/main/java/com/example/demo/Alien.java
@@ -7,21 +7,41 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+// 6. below annotation helps in creating object for following class 
+// 7. because of component we will have following output
+// object created
+// in show
+// in show
+
 @Component
+// @Scope(value = "prototype") 
+
+//=> bcoz of this 2 instances will be created => i.e when we ask for instance, by "getBean", then only it will create instance
+// 8. because of above will have following output
+// object created
+// in show
+// object created
+// in show
 public class Alien {
 
-	// below all are properties
+	//1.  below all are properties
 	private int aid;
 	private String aname;
 	private String tech;
 	@Autowired
 	@Qualifier("lap1")
 	private Laptop laptop;
+
+	
 	
 	public Alien() {
 		super();
 		System.out.println("object created..");
 	}
+
+
+    //2.  below ones are getters and setters, created by ide itself, right click> source> generate getters and setters
+
 	public int getAid() {
 		return aid;
 	}
@@ -46,6 +66,9 @@ public class Alien {
 	public void setLaptop(Laptop laptop) {
 		this.laptop = laptop;
 	}
+
+	//3. this method is added, so that we can once check if ConfigurableApplicationContext is working fo creating object
+	// 4. check for diagram on one note, to check how it is working...
 	public void show() 
 	{
 		System.out.println("in show");
--- 2_demo/src/main/java/com/example/demo/DemoApplication.java
+++ 2_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,10 +8,16 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context =  SpringApplication.run(DemoApplication.class, args);
+		// 5. container of springbean gets created bcoz of just below line, 
+		// the moment it gets to ".run" it returns you the context , this is where it initialises your container
+		// in this container, it will try to create all objects of those classes, where @Component annotation will be written
+		// 6. the moment below line runs, it will create object for the class where @Component was mentioned
+		ConfigurableApplicationContext context =  SpringApplication.run(DemoApplication.class, args); // here the run method, returns object of ConfigurableApplicationContext class, so run method returns object of this one
 		Alien a = context.getBean(Alien.class); // getBean refers to getting object of Alien class
-		a.show();
+		a.show(); // a is object of Alien class, and show is method of class Alien , so with a object we will be calling show method
 
+		Alien a1 = context.getBean(Alien.class); 
+		a1.show();
 	}
 
 }
